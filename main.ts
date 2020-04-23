3d1138351b0e81b2f43ff519280ecb353bdfc795
Jack <58662374+TheRedFlash5279@users.noreply.github.com>
2020-04-23 08:29:34 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const player2 = SpriteKind.create()
+}
 namespace myTiles {
     //% blockIdentity=images._tile
     export const tile0 = img`
@@ -90,6 +93,129 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     )
     mySprite.setVelocity(0, 80)
 })
+controller.player2.onButtonEvent(ControllerButton.Down, ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite5,
+    [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . f 1 1 1 1 1 1 1 f . . . . 
+. . . f 1 f 1 1 f f 1 f . . . . 
+. . . f 1 1 f 1 f f 1 f . . . . 
+. . . f 1 1 1 1 1 1 1 f . . . . 
+. . . f 1 1 1 1 1 c 1 f . . . . 
+. . . f c c 1 c c c c f . . . . 
+. . . f c c c c c c c f . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`],
+    100,
+    false
+    )
+})
+sprites.onOverlap(SpriteKind.player2, SpriteKind.Food, function (sprite, otherSprite) {
+    tiles.placeOnRandomTile(mySprite2, sprites.castle.tileGrass2)
+    info.changeScoreBy(1)
+    if (info.score() == 1) {
+        mySprite3 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . . f f f f f f f . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Enemy)
+        mySprite3.follow(mySprite, 75)
+        info.changeLifeBy(1)
+    }
+    if (info.score() == 2) {
+        MySprite4 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . . f f f f f f f . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Enemy)
+        MySprite4.follow(mySprite, 65)
+        info.changeLifeBy(1)
+    }
+    if (info.score() == 3) {
+        mySprite3 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . . f f f f f f f . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Enemy)
+        mySprite3.follow(mySprite, 55)
+        info.changeLifeBy(1)
+    }
+    if (info.score() == 4) {
+        mySprite3 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . . f f f f f f f . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Enemy)
+        mySprite3.follow(mySprite, 45)
+        info.changeLifeBy(1)
+    }
+    if (info.score() == 5) {
+        mySprite.say("I'm to full!", 2000)
+        info.changeLifeBy(1)
+        info.changeScoreBy(-1)
+        info.changeLifeBy(-1)
+    }
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     tiles.placeOnRandomTile(mySprite2, sprites.castle.tileGrass2)
     info.changeScoreBy(1)
@@ -113,19 +239,20 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSpr
 . . . . . . . . . . . . . . . . 
 `, SpriteKind.Enemy)
         mySprite3.follow(mySprite, 75)
+        info.changeLifeBy(1)
     }
     if (info.score() == 2) {
         MySprite4 = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . f f f f f f f . . . . 
-. . . . f 8 8 8 8 8 8 8 f . . . 
-. . . . f 8 8 8 8 8 8 8 f . . . 
-. . . . f 8 8 8 8 8 8 8 f . . . 
-. . . . f 8 8 8 8 8 8 8 f . . . 
-. . . . f 8 8 8 8 8 8 8 f . . . 
-. . . . f 8 8 8 8 8 8 8 f . . . 
-. . . . f 8 8 8 8 8 8 8 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
 . . . . . f f f f f f f . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -134,19 +261,20 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSpr
 . . . . . . . . . . . . . . . . 
 `, SpriteKind.Enemy)
         MySprite4.follow(mySprite, 65)
+        info.changeLifeBy(1)
     }
     if (info.score() == 3) {
         mySprite3 = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . f f f f f f f . . . . 
-. . . . f c c c c c c c f . . . 
-. . . . f c c c c c c c f . . . 
-. . . . f c c c c c c c f . . . 
-. . . . f c c c c c c c f . . . 
-. . . . f c c c c c c c f . . . 
-. . . . f c c c c c c c f . . . 
-. . . . f c c c c c c c f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
 . . . . . f f f f f f f . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -155,19 +283,20 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSpr
 . . . . . . . . . . . . . . . . 
 `, SpriteKind.Enemy)
         mySprite3.follow(mySprite, 55)
+        info.changeLifeBy(1)
     }
     if (info.score() == 4) {
         mySprite3 = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . f f f f f f f . . . . 
-. . . . f 5 5 5 5 5 5 5 f . . . 
-. . . . f 5 5 5 5 5 5 5 f . . . 
-. . . . f 5 5 5 5 5 5 5 f . . . 
-. . . . f 5 5 5 5 5 5 5 f . . . 
-. . . . f 5 5 5 5 5 5 5 f . . . 
-. . . . f 5 5 5 5 5 5 5 f . . . 
-. . . . f 5 5 5 5 5 5 5 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 f . . . 
 . . . . . f f f f f f f . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -176,10 +305,64 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSpr
 . . . . . . . . . . . . . . . . 
 `, SpriteKind.Enemy)
         mySprite3.follow(mySprite, 45)
+        info.changeLifeBy(1)
     }
     if (info.score() == 5) {
-        mySprite.say("I'm to full to eat!", 2000)
+        mySprite.say("I'm to full!", 2000)
+        info.changeLifeBy(1)
+        info.changeScoreBy(-1)
+        info.changeLifeBy(-1)
     }
+})
+controller.player2.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite5,
+    [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . f c c 1 1 1 1 1 f . . . . 
+. . . f c 1 1 1 1 f 1 f . . . . 
+. . . f c 1 1 1 f 1 1 f . . . . 
+. . . f c c 1 1 1 1 1 f . . . . 
+. . . f c c 1 1 f f 1 f . . . . 
+. . . f c c c 1 f f 1 f . . . . 
+. . . f c c 1 1 1 1 1 f . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`],
+    100,
+    false
+    )
+})
+controller.player2.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite5,
+    [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . f c c c c c c c f . . . . 
+. . . f c c c c 1 1 c f . . . . 
+. . . f 1 c 1 1 1 1 1 f . . . . 
+. . . f 1 1 1 1 1 1 1 f . . . . 
+. . . f 1 f f 1 f 1 1 f . . . . 
+. . . f 1 f f 1 1 f 1 f . . . . 
+. . . f 1 1 1 1 1 1 1 f . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`],
+    100,
+    false
+    )
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
@@ -206,6 +389,31 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     false
     )
     mySprite.setVelocity(80, 0)
+})
+controller.player2.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite5,
+    [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . f 1 1 1 1 1 c c f . . . . 
+. . . f 1 f 1 1 c c c f . . . . 
+. . . f 1 1 f 1 1 c c f . . . . 
+. . . f 1 1 1 1 1 c c f . . . . 
+. . . f 1 f f 1 1 1 c f . . . . 
+. . . f 1 f f 1 1 1 c f . . . . 
+. . . f 1 1 1 1 1 c c f . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`],
+    100,
+    false
+    )
 })
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
@@ -235,8 +443,10 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 let MySprite4: Sprite = null
 let mySprite3: Sprite = null
+let mySprite5: Sprite = null
 let mySprite2: Sprite = null
 let mySprite: Sprite = null
+info.setLife(1)
 mySprite = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -281,21 +491,39 @@ tiles.setTilemap(tiles.createTilemap(
 mySprite.setPosition(79, 24)
 mySprite.setFlag(SpriteFlag.StayInScreen, true)
 mySprite2 = sprites.create(img`
-3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . f c f . . . . . . . 
+. . . . . f c 2 c f . . . . . . 
+. . . . . f 2 2 2 f . . . . . . 
+. . . . . f c 2 c f . . . . . . 
+. . . . . . f c f . . . . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
 `, SpriteKind.Food)
 mySprite2.setPosition(73, 57)
+mySprite5 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . f c c c c c c c f . . . . 
+. . . f c 1 1 c c c c f . . . . 
+. . . f 1 1 1 1 1 c 1 f . . . . 
+. . . f 1 1 1 1 1 1 1 f . . . . 
+. . . f 1 f f 1 f 1 1 f . . . . 
+. . . f 1 f f 1 1 f 1 f . . . . 
+. . . f 1 1 1 1 1 1 1 f . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.player2)
